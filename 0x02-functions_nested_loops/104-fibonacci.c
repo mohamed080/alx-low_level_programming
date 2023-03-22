a9c364d50bedffcb3910f7c49c57bda355ac9fbd
mohamed080 <mohayman080@gmail.com>
2023-03-21 19:31:54 -0700

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,9 +2,31 @@
 #include <stdio.h>
 
 /**
+ * numLength - returns the length of string
+ *
+ * @num: operand number
+ *
+ * Return: number of digits
+*/
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
+}
+
+/**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (success)
 */
 int main(void)
 {
