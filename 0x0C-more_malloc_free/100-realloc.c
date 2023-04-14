69da8f801f2a3264de5d45395737d574cbea2d74
mohamed080 <mohayman080@gmail.com>
2023-04-14 13:51:42 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,10 +19,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (new_size == old_size)
-		return (ptr);
-	if (new_size == 0 && ptr)
+		return (potr);
+	if (new_size == 0 && potr)
 	{
-		free(ptr);
+		free(potr);
+
 		return (NULL);
 	}
 	if (potr == 0)
