1281acae299506527954a9a7aa4647b5c7ce0f3f
mohamed080 <mohayman080@gmail.com>
2023-04-23 01:24:29 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - print a name
+ * @name: string 
+ * @f: pointer to function
+ * Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == '\0' || f == '\0')
+		return;
+
+	f(name);
+}
