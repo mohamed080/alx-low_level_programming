6f005568e9387f223e244545cdb4051ae8ad7d2d
mohamed080 <mohayman080@gmail.com>
2023-04-27 13:03:17 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+include "lists.h"
+
+void second(void) __attribute__ ((constructor));
+
+/**
+ * first - excute sentence before the main
+ * function is executed
+*/
+
+void second(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
