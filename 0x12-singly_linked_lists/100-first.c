c1a78fde6a36fda4a6b6341a4ae0ccb120b07b5c
mohamed080 <mohayman080@gmail.com>
2023-04-27 13:06:46 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void second(void) __attribute__ ((constructor));
+void excute(void) __attribute__ ((constructor));
 
 /**
- * first - excute sentence before the main
+ * excute - excute sentence before the main
  * function is executed
 */
 
-void second(void)
+void excute(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
