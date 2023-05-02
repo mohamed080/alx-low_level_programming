dbde38fc7f5d46d1012eb5704fb29096bb0dc747
mohamed080 <mohayman080@gmail.com>
2023-05-02 14:34:28 -0700

    102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop.
+ * @head: search for
+ *
+ * Return: address of the node where the loop starts.
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *sw = head;
+	listint_t *fast = head;
+
+	if (!head)
+		return (NULL);
+
+	while (sw && fast && fast->next)
+	{
+		fast = fast->next->next;
+		sw = sw->next;
+		if (fast == sw)
+		{
+			sw = head;
+			while (sw != fast)
+			{
+				sw = sw->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+
+	return (NULL);
+}
