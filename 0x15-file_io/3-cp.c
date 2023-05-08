34f5259ef67cd77a8afb41d5a8b1e4e6b0b8cfb5
mohamed080 <mohayman080@gmail.com>
2023-05-08 12:34:20 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,105 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+*/
+
+char *create_buffer(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buf);
+}
+
+/**
+ * close_file - Closes file
+ * @fn: The file descriptor to be closed.
+ */
+void close_file(int fn)
+{
+	int c;
+
+	c = close(fn);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fn);
+		exit(100);
+	}
+}
+
+/**
+ * main - Write a program that copies the content of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect one- exit code 97.
+ * If file_from does not exist or cannot be read it- exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
+*/
+
+int main(int argc, char *argv[])
+{
+	int from, o, r, w;
+	char *buf;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buf = create_buf(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	o = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buf);
+			exit(98);
+		}
+
+		w = write(o, buf, r);
+		if (o == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buf);
+			exit(99);
+		}
+
+		r = read(from, buf, 1024);
+		o = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buf);
+	close_file(from);
+	close_file(o);
+
+	return (0);
+}
